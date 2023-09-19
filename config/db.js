a69362f7247a33929a88ba48842f2d479f5822ca
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-19 13:58:31 +0300

    init sequelize orm as db

--- config/db.js
+++ config/db.js
@@ -1,1 +1,11 @@
+import { Sequelize } from 'sequelize';
 
+const db = new Sequelize({
+	dialect: 'mysql',
+	database: process.env.DB_NAME,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	host: process.env.DB_HOST,
+});
+
+export default db;
