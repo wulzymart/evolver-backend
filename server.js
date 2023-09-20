b763e5ab62f7c70678c1249d1a3e8c5d9c9f4225
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-20 05:20:43 +0300

    remove redundant server.js file

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-
-const server = http.createServer(app);
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
