6139c799abf34c05579d0ef08fdd9b76d837f4b0
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2023-09-20 19:42:33 +0200

    fix:ids

--- models/GroupMembership.js
+++ models/GroupMembership.js
@@ -4,11 +4,13 @@ import db from "../config/db.js";
 
 const GroupMembership = db.define("GroupMembership", {
   group_id: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
     allowNull: false,
   },
   user_id: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
     allowNull: false,
   },
 });
