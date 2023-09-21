88673d3d679303788f8e7ec55187ce0d60a06caa
MD ASIF HUSSAIN <mdasifhussain268@gmail.com>
2023-09-22 02:06:06 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { updateUser } from '../controllers/userController/index.js'
+import {removeInterest} from '../controllers/interestController/index.js'
 
 const userRouter = express.Router()
 
 userRouter.put('/user/:id', updateUser)
+userRouter.delete('/users/:userId/interests/:eventId', removeInterest)
 
 export default userRouter;
