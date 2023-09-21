07a296b7312ad06c093b051d7525753367c8d9ff
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2023-09-21 09:56:09 +0200

    fix pivots

--- models/Interest.js
+++ models/Interest.js
@@ -2,20 +2,30 @@ import {  DataTypes } from "sequelize";
 
 import db from "../config/db.js";
 
-const Interest = db.define("Interest", {
- event_id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
+const Interest = db.define(
+  "interest",
+  {
+    EventId: {
+      type: DataTypes.UUID,
+      references: {
+        model: Event,
+        key: "id",
+      },
+    },
+    UserId: {
+      type: DataTypes.UUID,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
   },
-  user_id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
-  },
-});
- Interest.belongsTo(interested_events, {
-   foreignKey: "event_id",
- });
+  {
+    tableName: "interested_events",
+    underscored: true,
+  }
+);
+User.belongsToMany(Event, { through: Interest });
+Event.belongsToMany(User, { through: Interest });
 
 export default Interest;
