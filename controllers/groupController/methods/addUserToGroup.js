176fa18c932eabbb0b47350894437d81357bfb3a
asahelcode <hoyx0101@gmail.com>
2023-09-21 09:16:56 +0100

    Add addUserToGroup controller

---
+++ controllers/groupController/methods/addUserToGroup.js
@@ -0,0 +1,21 @@
+import GroupMembership from "../../../models/GroupMembership";
+
+async function addUserToGroup(req, res) {
+  const { userId, groupId } = req.params;
+
+  try {
+    GroupMembership.create({
+      user_id: userId,
+      group_id: groupId,
+    });
+
+    res
+      .status(200)
+      .json({ message: "user has been successfully added to group." });
+  } catch (err) {
+    console.error("Error creating user: ", err);
+    res.status(500).json({ message: "Internal server error." });
+  }
+}
+
+export default addUserToGroup;
