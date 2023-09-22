e05fadd40b9ba5816fd27703a12b45d4d1449089
Olawole Olawale Martins <olawalemart@gmail.com>
2023-09-23 00:05:23 +0100

    reverted to ayines comments correction

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,6 +4,6 @@ import { userAuthorisation } from "../middleware/authorization.js";
 
 const commentRouter = express.Router();
 
-commentRouter.get("/events/:eventId/comment", userAuthorisation, listComments);
+commentRouter.get("/events/:eventId/comments", userAuthorisation, listComments);
 
 export default commentRouter;
