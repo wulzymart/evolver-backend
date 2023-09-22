5664047e49e98ae3fca81eac7ed81bd72d06a63c
Udeorah Frank <franklynudeorah@gmail.com>
2023-09-22 09:41:47 +0100

    add route to update group

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -3,8 +3,8 @@ import { addUserToGroup, removeUserFromGroup, createGroup } from "../controllers
 
 const groupRouter = express.Router();
 
-groupRouter.put("/groups/:groupId")
 groupRouter.post("/groups", createGroup);
+groupRouter.put("/groups/:groupId")
 groupRouter.post("/groups/:groupId/members/:userId", addUserToGroup);
 groupRouter.delete("/groups/:groupId/members/:userId", removeUserFromGroup);
 
