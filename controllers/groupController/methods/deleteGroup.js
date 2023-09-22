16fadb7738a8546a9baded3690a55d939f89575b
Carlson Oranu <oranucarl@gmail.com>
2023-09-22 10:14:06 +0100

    First deleteGroup implementation

--- controllers/groupController/methods/deleteGroup.js
+++ controllers/groupController/methods/deleteGroup.js
@@ -1,1 +1,32 @@
- 
+import { StatusCodes } from "http-status-codes";
+import Group from "../../../models/Group.js";
+import tryCatchHelper from "../../../utils/helpers/tryCatch.helpers.js";
+import { errorResponse, successResponse } from "../../../utils/helpers/response.helpers.js";
+
+/**
+ * @description Delete a Group
+ * @route DELETE /api/groups/:groupId
+ * @param {object} req - Request object
+ * @param {object} res - Response object
+ * @returns {object} Response message
+ */
+
+const deleteGroup = tryCatchHelper(async (req, res) => {
+  const { groupId } = req.params;
+
+  if (!groupId) {
+    return errorResponse(res, "Invalid group ID", StatusCodes.BAD_REQUEST);
+  }
+
+  const group = await Group.findByPk(groupId);
+
+  if (!group) {
+    return errorResponse(res, "Group not found", StatusCodes.NOT_FOUND);
+  }
+
+  await group.destroy();
+
+  successResponse(res, `${group.title} deleted successfully`, null, StatusCodes.NO_CONTENT);
+});
+
+export default deleteGroup;
