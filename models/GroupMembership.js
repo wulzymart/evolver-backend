71480e23d6d663af54d05f52a9c664a949286c1b
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-22 04:30:27 +0300

    update groupMembership model

--- models/GroupMembership.js
+++ models/GroupMembership.js
@@ -1,18 +1,34 @@
-import { Sequelize, DataTypes } from "sequelize";
+import { DataTypes } from "sequelize";
+import User from "./User.js";
+import Group from "./Group.js";
 
 import db from "../config/db.js";
 
-const GroupMembership = db.define("GroupMembership", {
-  group_id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
+const GroupMembership = db.define(
+  "GroupMembership",
+  {
+    group_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: Group,
+        key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
   },
-  user_id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
+  {
+    tableName: "user_groups",
+    timestamps: false,
   },
-});
+);
+
+User.belongsToMany(Group, { through: GroupMembership, foreignKey: "user_id" });
+Group.belongsToMany(User, { through: GroupMembership, foreignKey: "group_id" });
 
 export default GroupMembership;
