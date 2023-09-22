b94aa199bf19a6820e5fa339e69af45d63c10ca9
Carlson Oranu <oranucarl@gmail.com>
2023-09-22 10:40:57 +0100

    Import and export deleteGroup method on index file

--- controllers/groupController/index.js
+++ controllers/groupController/index.js
@@ -1,5 +1,6 @@
 import removeUserFromGroup from "./methods/removeUserFromGroup.js";
 import addUserToGroup from "./methods/addUserToGroup.js";
 import createGroup from "./methods/createGroup.js";
+import deleteGroup from "./methods/deleteGroup.js";
 
-export { addUserToGroup, removeUserFromGroup, createGroup };
+export { addUserToGroup, removeUserFromGroup, createGroup, deleteGroup };
