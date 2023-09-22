773d2523a441fe7835ff8be7af1c09f70dadb527
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-22 17:22:00 +0300

    in-progress: delete group

--- controllers/groupController/methods/deleteGroup.js
+++ controllers/groupController/methods/deleteGroup.js
@@ -1,23 +1,74 @@
 import { StatusCodes } from "http-status-codes";
 import Group from "../../../models/Group.js";
 import tryCatchHelper from "../../../utils/helpers/tryCatch.helpers.js";
+import GroupMembership from "../../../models/GroupMembership.js";
+import GroupImage from "../../../models/GroupImage.js";
 
 const deleteGroup = tryCatchHelper(async (req, res) => {
   const { groupId } = req.params;
 
   if (!groupId) {
-    return res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid group ID" });
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Invalid group ID" });
   }
 
-  const group = await Group.findByPk(groupId); // Find the group
+  try {
+    const group = await Group.findByPk(groupId); // Find the group
 
-  if (!group) {
-    return res.status(StatusCodes.NOT_FOUND).json({ error: "Group not found" });
+    if (!group) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Group not found" });
+    }
+
+    // Delete all group memberships for this group
+    const deletedGroupMemberships = await GroupMembership.destroy({
+      where: {
+        group_id: group.id,
+      },
+    });
+
+    if (deletedGroupMemberships) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: "Unable to delete group memberships",
+      });
+    }
+
+    // Delete all group images for this group
+    const deletedGroupImages = await GroupImage.destroy({
+      where: {
+        group_id: group.id,
+      },
+    });
+
+    console.log(deletedGroupImages);
+
+    if (deletedGroupImages) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: "Unable to delete group images",
+      });
+    }
+
+    const deletedGroup = await group.destroy();
+
+    console.log(deletedGroup);
+
+    if (deletedGroup) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: "Unable to delete group",
+      });
+    }
+
+    return res.status(StatusCodes.OK).json({
+      message: `${group.title} deleted successfully`,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "Unable to delete group",
+    });
   }
-
-  await group.destroy();  // Delete the group
-
-  return res.status(StatusCodes.NO_CONTENT).json({ message: `${group.title} deleted successfully` });
 });
 
 export default deleteGroup;
--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { addUserToGroup, removeUserFromGroup, createGroup, getGroupDetails } from "../controllers/groupController/index.js";
+import {
+  addUserToGroup,
+  removeUserFromGroup,
+  createGroup,
+  getGroupDetails,
+  deleteGroup,
+} from "../controllers/groupController/index.js";
 
 const groupRouter = express.Router();
 
 groupRouter.post("/groups", createGroup);
-groupRouter.put("/groups/:groupId")
+groupRouter.put("/groups/:groupId");
+groupRouter.delete("/groups/:groupId", deleteGroup);
 groupRouter.post("/groups/:groupId/members/:userId", addUserToGroup);
 groupRouter.delete("/groups/:groupId/members/:userId", removeUserFromGroup);
 groupRouter.get("/groups/:groupId", getGroupDetails);
