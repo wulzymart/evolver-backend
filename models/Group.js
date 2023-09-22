714f521a232d70d20e3125749e9432ac2eec9b64
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-22 04:15:59 +0300

    create group model

---
+++ models/Group.js
@@ -0,0 +1,31 @@
+import db from "../config/db.js";
+import { DataTypes } from "sequelize";
+
+const Group = db.define(
+  "groups",
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+      onUpdate: DataTypes.NOW,
+    },
+  },
+  { timestamps: false },
+);
+
+export default Group;
