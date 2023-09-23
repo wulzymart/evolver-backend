098a9bf0addd2e9a3a0a34a511bb29f34e31f02e
Olawole Olawale Martins <olawalemart@gmail.com>
2023-09-23 18:27:46 +0100

    editted logout route to return a payload instead of redirect

--- controllers/userController/methods/logoutUser.js
+++ controllers/userController/methods/logoutUser.js
@@ -1,6 +1,7 @@
-function logout(req, res,next) {
-    req.logout();
-    res.redirect('/');
-  }
-  
-  export default logout;
+import { successResponse } from "../../../utils/helpers/response.helpers.js";
+function logout(req, res, next) {
+  req.logout();
+  return successResponse(res, "success", { loggedOut: true }, 200);
+}
+
+export default logout;
